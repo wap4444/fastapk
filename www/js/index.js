8437e1915583ce41d001b426b5dfcde3cc4ced8e
wap4444 <wap4444@mail.ru>
2017-11-19 13:29:26 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -19,69 +19,28 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-	    document.addEventListener("offline", onOffline, false);
+document.addEventListener("offline", onOffline, false);
 function onOffline() {
 $('#BtnEnt').hide();
 $('#inetOff').show();
-var ref = cordova.InAppBrowser.open('http://smarthomealma.controlsoft.kz/fr7/index.html?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/index.html?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 ref.close();
 }
 document.addEventListener("online", onOnline, false);
  
 function onOnline() {
-var ref = cordova.InAppBrowser.open('http://smarthomealma.controlsoft.kz/fr7/index.html?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/index.html?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 $('#inetOff').hide();
 $('#BtnEnt').show();
 }
 	    
-if(localStorage.ipush){}
-else{
-$('.loader1').css('height',screen.width+'px');
-$('.loader1').fadeIn();
-}
-		
-
-	    
-var i = 0;
-(function() {
-    if (i < 8) {
-var arr = ["Подготовка списка домов...", "Подготовка изображений...", "Загрузка иконок...", "Настройка данных..."];
-	    $('#loaderInfo').text(arr[i]);
-        i++;
-        setTimeout(arguments.callee, 5000);
-    } else {
- $('#loaderInfo').text("Настройка данных...");
-    } 
-})(); 
-	    
-
 $("#BtnEnt" ).click(function() {
-var ref = cordova.InAppBrowser.open('http://smarthomealma.controlsoft.kz/fr7/index.html?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/index.html?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 });
         
-function didReceiveRemoteNotificationCallBack(jsonData) {}
-function didOpenRemoteNotificationCallBack(jsonData) {}       
-        //Настройка ПУШЕЙ ДЛЯ АЙФОНА
-        var iosSettings = {};
-        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
-        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
-
-        //ПОДКЛЮЧЕНИЕ ПУШЕЙ 
-           window.plugins.OneSignal
-          .startInit("5ced1550-2eb8-4666-9d66-ef701a9c1b97")
-          .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
-          .handleNotificationOpened(didOpenRemoteNotificationCallBack)
-          .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
-          .iOSSettings(iosSettings)
-          .endInit();
-        
-window.plugins.OneSignal.getIds(function(ids) {
-ipush = ids.userId;
-$('.loader1').hide();
-localStorage.ipush=ipush;
-var ref = cordova.InAppBrowser.open('http://smarthomealma.controlsoft.kz/fr7/index.html?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/index.html?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 $('.loader2').show();
-});
+
         
 
     }
